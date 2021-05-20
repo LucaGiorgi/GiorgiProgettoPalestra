4c1c905e671a49ed11b8cd935dbd16d2b99c5ed1
LucaGiorgi <LucaGiorgi@localhost>
2021-05-20 12:51:25 +0200

    Completamento metodi principali

--- src/main/java/com/mycompany/palestra/Main.java
+++ src/main/java/com/mycompany/palestra/Main.java
@@ -5,6 +5,8 @@
  */
 package com.mycompany.palestra;
 
+import java.util.Scanner;
+
 /**
  *
  * @author LucaGiorgi
@@ -13,6 +15,48 @@ public class Main
 {
     public static void main(String[] args)
     {
+        int sceltaUtente =-1;
+        Scanner tastiera= new Scanner(System.in);
+        String[] vociMenu= new String[10];
+        Cliente c1 = new Cliente();
+        Cliente cliente;
+        String nomeFileCSV="Palestra.txt";
+        String nomeFileBinario="Palestra.bin";
         
+        vociMenu[0] = "aggiungi cliente";
+        vociMenu[1] = "Rimuovi cliente";
+        vociMenu[2] = "Visualizza tutti i clienti";
+        vociMenu[3] = "Visualizza cliente";
+        vociMenu[4] = "Visualizza clienti di un determinato corso";
+        vociMenu[5] = "Esporta i dati in formato CSV";
+        vociMenu[6] = "Esci";
+        Menu menu= new Menu(vociMenu);
+        
+         switch(sceltaUtente)
+            {
+                case 0:
+                {
+                    System.out.println("L'applicazione terminerà");
+                    break;
+                }
+                case 1:
+                {
+                    cliente = new Cliente();
+                    System.out.println("Codice identificativo --> ");
+                    cliente.setCodiceIdentificativo(tastiera.nextInt());
+                    System.out.println("Cognome --> ");
+                    cliente.setCognome(tastiera.nextLine());
+                    System.out.println("Nome --> ");
+                    cliente.setNome(tastiera.nextLine());
+                    /*
+                    System.out.println("Data --> ");
+                    cliente.setDataIscrizione(tastiera.);
+                    */
+                    System.out.println("Corso --> ");
+                    cliente.setCorso(tastiera.nextLine());
+                }
+        
+    }
+         
     }
 }
--- src/main/java/com/mycompany/palestra/Palestra.java
+++ src/main/java/com/mycompany/palestra/Palestra.java
@@ -11,5 +11,120 @@ package com.mycompany.palestra;
  */
 public class Palestra 
 {
+    private Cliente[] clienti;
+    private static int N_MAX_CLIENTI=1500;
+    //costruttore
+    public Palestra()
+     {
+       clienti = new Cliente[N_MAX_CLIENTI];
+     }
+
+    public Palestra(Cliente elencoClienti[])
+    {
+        clienti=new Cliente[N_MAX_CLIENTI];
+        int numeroClienti=0;
+        //se elenco libri non contiene libri
+        if(elencoClienti.length==0)
+            return;
+        if(elencoClienti.length>getN_MAX_CLIENTI())
+            numeroClienti=getN_MAX_CLIENTI();
+        else
+            numeroClienti=elencoClienti.length;
+        
+        for(int i=0;i<numeroClienti;i++)
+        {
+            if(elencoClienti[i]!=null)
+            {
+                clienti[i]=new Cliente(elencoClienti[i]);
+            }
+        }
+    }
+    public Cliente[] getClienti()
+    {
+        return clienti;
+    }
+
+    public static int getN_MAX_CLIENTI() 
+    {
+        return N_MAX_CLIENTI;
+    }
+    
+    public int getNumClienti()
+    {
+        int c=0;
+        for(int i=0; i<getN_MAX_CLIENTI();i++)
+        {
+            if (clienti[i]!=null)
+                c++;
+        }
+        return c;
+    }
+    
+    public Cliente getCliente(int posizione)
+    {
+        if(posizione<0 || posizione>=getN_MAX_CLIENTI())
+            return null;
+        if (clienti[posizione]==null)
+            return null;
+        return clienti[posizione];
+    }
+    
+    public int setCliente (Cliente cliente, int posizione)
+    {
+        if (posizione<0 || posizione>getNumClienti())
+            return -1;  //pos non valida*/
+        
+        try
+        {
+            if(clienti[posizione]!=null)
+            return -2;  //pos occupata
+        
+        //se posizione libera
+        clienti[posizione]=new Cliente(cliente);
+        return posizione;
+        }
+        catch(ArrayIndexOutOfBoundsException posizioneNonValida)
+        {
+                return -1;     
+        }
+        
+    }
+    public int rimuoviCliente(int codiceIdentificativo)
+    {
+        for(int i=0;i<getNumClienti();i++)
+        {
+            if(codiceIdentificativo== clienti[i].getCodiceIdentificativo())
+                clienti[i]=null;
+            else
+                return -1;
+        }
+                        return -2;
+    }
+        
+    public  String visualizzaClienti(Cliente clienti)
+    {
+        String s="";
+        if(this.getNumClienti() == 0)
+        {
+            s="Nessun cliente presete";
+        }
+        for(int i=0;i<this.getNumClienti();i++)
+        {
+            s=clienti.toString();
+        }
+        return s;
+    }
+    
+    public  int visualizzaCliente(String Cognome, String Nome)
+    {
+        String s="";
+        for(int i=0;i<getNumClienti();i++)
+        {
+            if(Cognome==clienti[i].getCognome() && Nome==clienti[i].getNome())
+                return 0; 
+        }
+        return -1;
+    }
+
     
 }
